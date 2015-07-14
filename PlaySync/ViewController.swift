3c122ac1df0e3fadce41eb490a9dd62c9373be8b
jamcar23 <jamcar23@gmail.com>
2015-07-13 21:42:18 -0400

    init xcode proj

---
+++ PlaySync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaySync
+//
+//  Created by James Carroll on 7/13/15.
+//  Copyright (c) 2015 James Carroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
